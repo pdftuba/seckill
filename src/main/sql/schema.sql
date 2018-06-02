d4544809971f3bfe724b11749f166d9fb6914311
pdf <p.df@foxmail.com>
2018-06-02 20:07:09 +0800

    修改mysql密码

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE seckill(
 `seckill_id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
 `name` varchar(120) NOT NULL COMMENT '商品名称',
 `number` int NOT NULL COMMENT '库存数量',
-`start_time` timestamp NOT NULL default '0000-00-00 00:00:00' COMMENT '秒杀开启时间',
-`end_time` timestamp NOT NULL default '0000-00-00 00:00:00' COMMENT '秒杀结束时间',
+`start_time` timestamp NOT NULL default '2000-01-01 00:00:00' COMMENT '秒杀开启时间',
+`end_time` timestamp NOT NULL default '2000-01-01 00:00:00' COMMENT '秒杀结束时间',
 `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 PRIMARY KEY (seckill_id),
 key idx_start_time(start_time),
@@ -19,14 +19,16 @@ key idx_create_time(create_time)
 )ENGINE =InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='秒杀库存表';
 
 
+
+
 --初始化数据
 insert into
   seckill(name,number,start_time,end_time)
 values
-  ('1000元秒杀iphone6',100,'2015-11-01 00:00:00','2015-11-02 00:00:00'),
-  ('500元秒杀ipad2',200,'2015-11-01 00:00:00','2015-11-02 00:00:00'),
-  ('300元秒杀小米4',300,'2015-11-01 00:00:00','2015-11-02 00:00:00'),
-  ('200元秒杀红米note',400,'2015-11-01 00:00:00','2015-11-02 00:00:00');
+  ('1000元秒杀iphone6',100,'2018-06-01 00:00:00','2018-12-12 00:00:00'),
+  ('500元秒杀ipad2',200,'2018-06-15 00:00:00','2019-06-15 00:00:00'),
+  ('300元秒杀小米4',300,'2017-11-21 00:00:00','2018-05-05 00:00:00'),
+  ('200元秒杀红米note',400,'2018-01-01 00:00:00','2018-08-30 00:00:00');
 
 --秒杀成功明细表
 --用户登录认证相关的信息
